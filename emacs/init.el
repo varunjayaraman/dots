7f803377223f70f3fa6f26e89d9444fcdec90da8
varun jayaraman <varun.jayaraman@gmail.com>
2020-12-19 17:57:46 +0100

    Add new emacs changes

--- emacs/init.el
+++ emacs/init.el
@@ -67,9 +67,13 @@
   (ivy-mode 1))
 
 (use-package counsel
-  :config
-  (counsel-mode 1))
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history)))
 
+(use-package doom-themes)
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
@@ -84,3 +88,98 @@
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+(use-package general
+  :config
+  (general-create-definer roonie/leader-keys
+  :keymaps '(normal insert visual emacs)
+  :prefix "SPC"
+  :global-prefix "C-SPC")
+  (roonie/leader-keys
+    "t" '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose theme"))
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t) ;; remaps C-u to scroll instead of doing universal argument
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  ;; Allows you to use C-h in insert mode as backspace, so you dont have to reach for the backspace key.
+  ;; However, if you do this, and you need to use the actual help version of C-h in insert mode, you will have
+  ;; to remember to exit to normal mode and then do C-h
+  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Moves to next or previous visual line (so if text is wrapped, it would go to the same line but the next visual one)
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package hydra)
+
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-increase "in")
+  ("k" text-scale-decrease "out")
+  ("f" nil "finished" :exit t))
+
+(roonie/leader-keys
+  "ts" '(hydra-text-scale/body :which-key "scale text"))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/workspace/code")
+    (setq projectile-project-search-path '("~/workspace/code"))))
+(setq projectile-switch-project-action #'projectile-dired))
+
+(setq ls-lisp-use-insert-directory-program nil)
+(require 'ls-lisp)
+
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package forge
+  :after magit)
+(setq auth-sources '("~/.authinfo"))
+
+(use-package super-save
+  :ensure t
+  :config
+  (super-save-mode +1))
+
+(setq auto-save-default nil)
+
+(use-package org)
