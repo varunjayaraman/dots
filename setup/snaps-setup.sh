00ee28d3f3c352551e6ba004b1a355178c59ad89
Varun Jayaraman <varun.jayaraman@gmail.com>
2021-01-05 09:17:33 +0100

    Add 1password snap and chrome

--- setup/snaps-setup.sh
+++ setup/snaps-setup.sh
@@ -5,5 +5,7 @@ sudo snap install telegram-desktop
 sudo snap install firefox
 sudo snap install emacs --classic
 sudo snap install slack --classic
+sudo snap install chrome
+sudo snap install --edge 1password # change once 1password on linux is stabled
 
 sudo snap refresh
