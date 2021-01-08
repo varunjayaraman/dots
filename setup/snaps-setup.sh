91942749c9ee9fc49229c1fd27ccf26f95b94aea
Varun Jayaraman <varun.jayaraman@gmail.com>
2021-01-08 22:29:44 +0100

    Add additional snap deps

--- setup/snaps-setup.sh
+++ setup/snaps-setup.sh
@@ -5,7 +5,8 @@ sudo snap install telegram-desktop
 sudo snap install firefox
 sudo snap install emacs --classic
 sudo snap install slack --classic
-sudo snap install chrome
+sudo snap install google-chrome
 sudo snap install --edge 1password # change once 1password on linux is stabled
+sudo snap install spotify
 
 sudo snap refresh
