6520d370d5d0ec6c800e8b455a19776bf06354ba
Varun Jayaraman <varun.jayaraman@gmail.com>
2021-01-12 23:09:06 +0100

    Fix/tsx prettier (#24)
    * TRy  LSP setup
    
    * fixes prettier issues with lsp mode

--- emacs/init.el
+++ emacs/init.el
@@ -265,30 +265,43 @@
     (add-hook 'before-save-hook #'roonie/lsp-format-buffer-quick nil t)))
 
 ;; Web
-(use-package web-mode
-  :mode (("\\.html?\\'" . web-mode)
-         ("\\.css\\'"   . web-mode)
-         ("\\.jsx?\\'"  . web-mode)
-         ("\\.tsx?\\'"  . web-mode)
-         ("\\.json\\'"  . web-mode))
+(use-package json-mode
+  :mode "\\.json$")
+(use-package web-mode  :ensure t
+  :mode (("\\.js\\'" . web-mode)
+         ("\\.jsx\\'" . web-mode)
+         ("\\.ts\\'" . web-mode)
+         ("\\.tsx\\'" . web-mode)
+         ("\\.html\\'" . web-mode)
+         ("\\.vue\\'" . web-mode)
+	     ("\\.json\\'" . web-mode))
+  :commands web-mode
+  :hook ((web-mode . lsp-deferred))
   :config
+  (setq company-tooltip-align-annotations t)
   (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-code-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
-  (setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'"))))
-(add-hook 'web-mode-hook
-          (lambda ()
-            (when (string-equal "tsx" (file-name-extension buffer-file-name))
-              (tide-setup)
-              (flycheck-mode t)
-              (setq flycheck-check-syntax-automatically '(save mode-enabled))
-              (eldoc-mode t)
-              (company-mode-on))))
+  (setq web-mode-code-indent-offset 2)
+  (setq web-mode-enable-part-face t)
+  (setq web-mode-content-types-alist
+	    '(("jsx" . "\\.js[x]?\\'")))
+  )
+(use-package add-node-modules-path)
 
 (eval-after-load 'web-mode
-    '(progn
-       (add-hook 'web-mode-hook #'add-node-modules-path)
-       (add-hook 'web-mode-hook #'prettier-js-mode)))
+  '(progn
+     (add-hook 'web-mode-hook #'add-node-modules-path)
+     (add-hook 'web-mode-hook #'prettier-mode)))
+(use-package prettier
+  :hook
+  ((typescript-mode json-mode) . prettier-mode))
+
+;; (use-package typescript-mode
+;;   :mode "\\.ts$"
+;;   :hook
+;;   (typescript-mode . lsp-deferred)
+;;   :custom
+;;   (typescript-indent-level 2))
 
 (use-package emmet-mode
   :hook ((html-mode       . emmet-mode)
@@ -303,27 +316,6 @@
                                (setq-local emmet-expand-jsx-className? t))))
 
 ;; Typescript & Javascript
-(use-package add-node-modules-path)
-(use-package prettier-js)
-(use-package typescript-mode)
-(use-package tide
-  :after (typescript-mode company flycheck)
-  :hook ((typescript-mode . tide-setup)
-         (typescript-mode . tide-hl-identifier-mode)
-         (before-save . tide-format-before-save)
-         (typescript-mode . yas-minor-mode)))
-
-(defun setup-tide-mode ()
-  (interactive)
-  (tide-setup)
-  (flycheck-mode +1)
-  (setq flycheck-check-syntax-automatically '(save mode-enabled))
-  (eldoc-mode +1)
-  (tide-hl-identifier-mode +1)
-  (company-mode +1))
-
-;; (add-hook 'before-save-hook 'tide-format-before-save)
-(add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 ;; Ruby
 (use-package inf-ruby)
