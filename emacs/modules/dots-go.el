f8982ea9fc213b81b6aec06a123bad2d011ceb48
Varun Jayaraman <varun.jayaraman@gmail.com>
2021-01-28 09:24:15 +0100

    Add gotest configuration

--- emacs/modules/dots-go.el
+++ emacs/modules/dots-go.el
@@ -13,6 +13,11 @@
          (go-mode . yas-minor-mode)
          (go-mode . lsp-deferred)))
 
+(use-package gotest
+  :bind (("C-c C-t s" . go-test-current-test)
+         ("C-c C-t f" . go-test-current-file)
+         ("C-c C-t a" . go-test-current-project)))
+
 ;; Set up before-save hooks to format buffer and add/delete imports.
 ;; Make sure you don't have other gofmt/goimports hooks enabled.
 (defun lsp-go-install-save-hooks ()
