1462fb15b75a37d09203aaabf82376bc01531f7f
Varun Jayaraman <varun.jayaraman@gmail.com>
2021-07-09 01:36:15 +0200

    Random tweaks

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -63,4 +63,7 @@ packer.startup(function()
 
   -- clojure
   use {"Olical/conjure", tag = 'v4.21.0'}
+  use {"guns/vim-sexp"}
+  use {"tpope/vim-sexp-mappings-for-regular-people"}
+
 end)
--- setup/install_clojure.sh
+++ setup/install_clojure.sh
@@ -7,6 +7,11 @@ function install_clojure() {
   curl -O https://download.clojure.org/install/linux-install-$clojure_version.sh
   chmod +x linux-install-$clojure_version.sh
   sudo ./linux-install-$clojure_version.sh
+
+  # install lein
+  curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > lein
+  sudo mv lein /usr/local/bin/lein
+  sudo chmod +x /usr/local/bin/lein
 }
 
 install_clojure
